5ae04289c8a027f929e84b410dfb7db2b98b3425
wary <waryewe>
2025-03-29 00:56:33 +0100

    Changed the d key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,17 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-for i = 1, 9 do
-  vim.keymap.set(
-    "n",
-    "<A-" .. i .. ">",
-    ':lua require("bufferline").go_to_buffer(' .. i .. ", true)<CR>",
-    { noremap = true, silent = true }
-  )
-end
+--for i = 1, 9 do
+--  vim.keymap.set(
+--    "n",
+--    "<A-" .. i .. ">",
+--    ':lua require("bufferline").go_to_buffer(' .. i .. ", true)<CR>",
+--    { noremap = true, silent = true }
+--  )
+--end
 --vim.keymap.set("n", "<C-W>", ":bd<CR>", { noremap = true, silent = true })
 
 -- Remap the Delete key to delete without using the clipboard
-vim.api.nvim_set_keymap("n", "<Del>", '"_x', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<Del>", '<C-O>"_x', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<Del>", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "d", '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "d", '"_d', { noremap = true, silent = true })
